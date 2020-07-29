eb3f730fc18dfc63f9228f3919b0eb4244596402
Yaro <yshmarov@gmail.com>
2020-07-29 17:29:43 +0000

    Added devise and functional navbar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.hosts << "7013294b8f6b4f15b3c8a1b9fc468a09.vfs.cloud9.us-east-1.amazonaws.com"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
