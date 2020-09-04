0292ea60401de84c9c6bf8fb3ecd177c61e55b5b
Yaro <yshmarov@gmail.com>
2020-09-04 11:44:18 +0000

    added user policies for controller actions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,7 @@ class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
   def index
+    
     # if params[:title]
     #   @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
     # else
@@ -10,6 +11,7 @@ class CoursesController < ApplicationController
     #   # @q = Course.ransack(params[:q])
     #   # @courses = @q.result.includes(:user)
     # end
+    
       @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
       @courses = @ransack_courses.result.includes(:user)
   end
@@ -22,16 +24,19 @@ class CoursesController < ApplicationController
   # GET /courses/new
   def new
     @course = Course.new
+    authorize @course
   end
 
   # GET /courses/1/edit
   def edit
+    authorize @course
   end
 
   # POST /courses
   # POST /courses.json
   def create
     @course = Course.new(course_params)
+    authorize @course
     @course.user = current_user
 
     respond_to do |format|
@@ -48,6 +53,7 @@ class CoursesController < ApplicationController
   # PATCH/PUT /courses/1
   # PATCH/PUT /courses/1.json
   def update
+    authorize @course
     respond_to do |format|
       if @course.update(course_params)
         format.html { redirect_to @course, notice: 'Course was successfully updated.' }
@@ -60,6 +66,7 @@ class CoursesController < ApplicationController
   end
 
   def destroy
+    authorize @course
     @course.destroy
     respond_to do |format|
       format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
