b39596f06484948e9ccb9c2a847d3d89b0a0fa2c
Yaro <yshmarov@gmail.com>
2020-09-22 14:28:14 +0000

    Added lessons model and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
          
@@ -16,6 +14,9 @@ class User < ApplicationRecord
   
   has_many :courses
   
+  extend FriendlyId
+  friendly_id :email, use: :slugged
+  
     after_create :assign_default_role
 
   def assign_default_role
@@ -30,9 +31,13 @@ class User < ApplicationRecord
   end
   
   validate :must_have_a_role, on: :update
+
+  def online?
+    updated_at > 2.minutes.ago
+  end
   
   private
-  
+
   def must_have_a_role
     unless roles.any?
       errors.add(:roles, "must have at least one role")
