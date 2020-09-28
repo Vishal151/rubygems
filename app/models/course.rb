53b807d7201f6d448dad5e416c44da36f02daddf
Yaro <vishal.patel@sidewayseight.co.uk>
2020-09-28 04:31:47 +0000

    Move logic from controller to model using scopes

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,9 +6,13 @@ class Course < ApplicationRecord
   #User.find_each { |user| User.reset_counters(user.id, :courses) }  
   has_many :lessons, dependent: :destroy
   has_many :enrollments
-  
+
   validates :title, uniqueness: true
-  
+
+  scope :latest, -> { limit(3).order(created_at: :desc) }
+  scope :top_rated, -> { limit(3).order(average_rating: :desc, created_at: :desc) }
+  scope :popular, -> { limit(3).order(enrollments_count: :desc, created_at: :desc) }
+
   def to_s
     title
   end
@@ -16,24 +20,24 @@ class Course < ApplicationRecord
 
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
+
   LANGUAGES = [:"English", :"Russian", :"Polish", :"Spanish"]
   def self.languages
     LANGUAGES.map { |language| [language, language] }
   end
-  
-    LEVELS = [:"Beginner", :"Intermediate", :"Advanced"]
+
+  LEVELS = [:"Beginner", :"Intermediate", :"Advanced"]
   def self.levels
     LEVELS.map { |level| [level, level] }
   end
-  
+
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user }
-  
+
   def bought(user)
     self.enrollments.where(user_id: [user.id], course_id: [self.id]).empty?
   end
-  
+
   def update_rating
     if enrollments.any? && enrollments.where.not(rating: nil).any?
       update_column :average_rating, (enrollments.average(:rating).round(2).to_f)
@@ -41,5 +45,5 @@ class Course < ApplicationRecord
       update_column :average_rating, (0)
     end
   end
-  
+
 end
