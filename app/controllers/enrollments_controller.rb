0e9246b642d74da8e7b9adb077d6e0f42a6a60b4
Yaro <vishal.patel@sidewayseight.co.uk>
2020-10-06 13:59:21 +0000

    action_mailler teacher gets notofied of new enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -48,7 +48,8 @@ class EnrollmentsController < ApplicationController
   def create
       @enrollment = current_user.buy_course(@course)
       redirect_to course_path(@course), notice: "You are enrolled!"
-      EnrollmentMailer.new_enrollment(@enrollment).deliver_later
+      EnrollmentMailer.student_enrollment(@enrollment).deliver_later
+      EnrollmentMailer.teacher_enrollment(@enrollment).deliver_later
   end
 
   def update
